7ca586c61fd686b5be2de19117ba4704ed1b688d
Yassir Najmaoui <43155068+yassirnajmaoui@users.noreply.github.com>
2021-03-16 11:08:22 -0400

    Prepare menus

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-# Python tkinter hello world program
-
+# From https://www.python-course.eu/tkinter_menus.php
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
+def NewFile():
+    print("New File!")
+def OpenFile():
+    name = askopenfilename()
+    print(name)
+def About():
+    print("This is a simple example of a menu")
+    
 root = Tk()
-a = Label(root, text ="Hello World")
-a.pack()
- 
-root.mainloop()
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=NewFile)
+filemenu.add_command(label="Open...", command=OpenFile)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
 
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=About)
+
+mainloop()
